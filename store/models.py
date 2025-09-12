a1d8549ab2d8f4a31fc105567eecf7de7f009948
sudharsanKEC <sudharsans913@gmail.com>
2025-09-12 06:45:10 +0530

    Wishlist migrated successfully

--- store/models.py
+++ store/models.py
@@ -142,5 +142,13 @@ class Order(models.Model):
     def __str__(self):
         return f"{self.customer.name} - {self.product.name} ({self.status})"
 
+class Wishlist(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ("customer","product") # prevents duplicate wishlist
+    def __str__(self):
+        return f"{self.customer.name} - {self.product.name}"    
 
